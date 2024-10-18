77f9fd95dab7b7b6b3df450a75aeb9b816f68fd5
Ericlys Moreira <ericlysm@gmail.com>
2024-10-18 08:16:12 -0300

    chore: add secret

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -7,7 +7,6 @@ export class AppController {
 
   @Get()
   getHello(): string {
-    console.log(process.env.APP);
     return this.appService.getHello();
   }
 
--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,9 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
+    console.log(`ConfigMap: ${process.env.APP}`);
+    console.log(`Secret: ${process.env.API_KEY}`);
+
     return 'Hello World!';
   }
 
